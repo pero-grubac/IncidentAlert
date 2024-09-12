a1eb313b173d9d69eed1d9feecf5a2c64bcdc4aa
Pero <perogrubac99@gmail.com>
2024-09-12 23:44:41 +0200

    Location repository

---
+++ IncidentAlert/Repositories/ILocationRepository.cs
@@ -0,0 +1,16 @@
+﻿using IncidentAlert.Models;
+using System.Linq.Expressions;
+
+namespace IncidentAlert.Repositories
+{
+    public interface ILocationRepository
+    {
+        Task<IEnumerable<Location>> GetAllAsync();
+        Task<Location?> GetByIdAsync(int id);
+        Task<Location> AddAsync(Location location);
+        Task<Location> UpdateAsync(Location location);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(Expression<Func<Location, bool>> predicate);
+        Task<IEnumerable<Location>> FindAsync(Expression<Func<Location, bool>> predicate);
+    }
+}
---
+++ IncidentAlert/Repositories/Implementation/LocationRepository.cs
@@ -0,0 +1,50 @@
+﻿using IncidentAlert.Data;
+using IncidentAlert.Exceptions;
+using IncidentAlert.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace IncidentAlert.Repositories.Implementation
+{
+    public class LocationRepository(DataContext dataContext) : ILocationRepository
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        public async Task<Location> AddAsync(Location location)
+        {
+            await _dataContext.Locations.AddAsync(location);
+            await _dataContext.SaveChangesAsync();
+            return location;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dataContext.Locations.FirstOrDefaultAsync(x => x.Id == id) ?? throw new EntityDoesNotExistException($"Entity with ID {id} does not exist.");
+            try
+            {
+                _dataContext.Locations.Remove(entity);
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new EntityCannotBeDeletedException($"Entity with ID {id} cannot be deleted. {ex.Message}", ex);
+            }
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<Location, bool>> predicate) => await _dataContext.Locations.AnyAsync(predicate);
+
+        public async Task<IEnumerable<Location>> FindAsync(Expression<Func<Location, bool>> predicate) => await _dataContext.Locations.Where(predicate).ToListAsync();
+
+
+        public async Task<IEnumerable<Location>> GetAllAsync() => await _dataContext.Locations.ToListAsync();
+
+        public async Task<Location?> GetByIdAsync(int id) => await _dataContext.Locations.FirstOrDefaultAsync(e => e.Id == id);
+
+        public async Task<Location> UpdateAsync(Location location)
+        {
+            _dataContext.Locations.Update(location);
+            await _dataContext.SaveChangesAsync();
+            return location;
+        }
+    }
+}
