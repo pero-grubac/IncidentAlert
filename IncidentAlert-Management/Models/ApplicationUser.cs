9ae530e06db1f8eab822bf720e745e6004aa2870
Pero <perogrubac99@gmail.com>
2024-09-21 17:19:28 +0200

    ApplicationUser

---
+++ IncidentAlert-Management/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IncidentAlert_Management.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public RoleEnum Role { get; set; }
+
+    }
+}
