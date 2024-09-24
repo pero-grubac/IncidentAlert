3d365386762f5d8074715c57d7e4b5a4a40f7f1b
Pero <perogrubac99@gmail.com>
2024-09-24 15:27:34 +0200

    incident_alert_mangment database created jwt configured user configured

---
+++ IncidentAlert-Management/Data/DataContext.cs
@@ -0,0 +1,61 @@
+﻿using IncidentAlert.Models;
+using IncidentAlert_Management.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IncidentAlert_Management.Data
+{
+    public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<IncidentCategory> IncidentCategories { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+               .HasIndex(c => c.Name)
+               .IsUnique();
+
+            modelBuilder.Entity<IncidentCategory>()
+                .HasKey(ic => new { ic.IncidentId, ic.CategoryId });
+
+            modelBuilder.Entity<IncidentCategory>()
+                .HasOne(ic => ic.Incident)
+                .WithMany(i => i.IncidentCategories)
+                .HasForeignKey(ic => ic.IncidentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<IncidentCategory>()
+                .HasOne(ic => ic.Category)
+                .WithMany(c => c.IncidentCategories)
+                .HasForeignKey(ic => ic.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Location>()
+                .HasMany(l => l.Incidents)
+                .WithOne(i => i.Location)
+                .HasForeignKey(i => i.LocationId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Incident>()
+                .HasOne(i => i.Location)
+                .WithMany(l => l.Incidents)
+                .HasForeignKey(i => i.LocationId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Location>()
+                .HasIndex(l => l.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Image>()
+                .HasOne<Incident>()
+                .WithMany(i => i.Images)
+                .HasForeignKey(i => i.IncidentId);
+        }
+    }
+}
--- IncidentAlert-Management/Program.cs
+++ IncidentAlert-Management/Program.cs
@@ -1,3 +1,13 @@
+using IncidentAlert_Management.Data;
+using IncidentAlert_Management.JWT;
+using IncidentAlert_Management.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,75 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database
+builder.Services.AddDbContext<DataContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("IncidentAlertManagementDB")));
+
+// User
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+})
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddSingleton(TimeProvider.System);
+
+// CORS
+var frontendUrl = builder.Configuration.GetValue<string>("FrontendUrl");
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins(frontendUrl)
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+// Logging
+builder.Host.UseSerilog((context, loggerConfig) =>
+{
+    loggerConfig.ReadFrom.Configuration(context.Configuration);
+});
+
+// Security
+var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+builder.Services.AddSingleton(jwtSettings);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+    };
+});
 
 var app = builder.Build();
 
