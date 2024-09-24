52af213bb7750e1ecc63c429e81c4e9b4c9aa78f
Pero <perogrubac99@gmail.com>
2024-09-24 19:51:51 +0200

    UserRepository

--- IncidentAlert-Management/Models/Dto/UserDto.cs
+++ IncidentAlert-Management/Models/Dto/UserDto.cs
@@ -5,5 +5,6 @@
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public RoleEnum Role { get; set; }
     }
 }
---
+++ IncidentAlert-Management/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using IncidentAlert_Management.Data;
+using IncidentAlert_Management.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace IncidentAlert_Management.Repositories.Implementation
+{
+    public class UserRepository(DataContext dataContext) : IUserRepository
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        public async Task Add(ApplicationUser user)
+        {
+            await _dataContext.Users.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteByUsername(string username)
+        {
+            var user = await GetByUsername(username);
+            if (user != null)
+            {
+                _dataContext.Users.Remove(user);
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> Exists(Expression<Func<ApplicationUser, bool>> predicate)
+             => await _dataContext.Users.AnyAsync(predicate);
+
+        public async Task<ApplicationUser?> Find(Expression<Func<ApplicationUser, bool>> predicate)
+             => await _dataContext.Users.FirstOrDefaultAsync(predicate);
+
+        public async Task<IEnumerable<ApplicationUser>> FindAll(Expression<Func<ApplicationUser, bool>> predicate)
+             => await _dataContext.Users.Where(predicate).ToListAsync();
+
+
+        public async Task<IEnumerable<ApplicationUser>> GetAll()
+             => await _dataContext.Users.ToListAsync();
+
+        public async Task<ApplicationUser?> GetById(string id)
+            => await Find(e => e.Id == id);
+
+        public async Task<ApplicationUser?> GetByUsername(string username)
+            => await Find(e => e.UserName == username);
+
+        public async Task Update(ApplicationUser user)
+        {
+            _dataContext.Users.Update(user);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
