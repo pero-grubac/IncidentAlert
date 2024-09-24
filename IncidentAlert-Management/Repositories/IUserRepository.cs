958e6dcffb99867c833463e42fad1fc3dfcbaaea
Pero <perogrubac99@gmail.com>
2024-09-24 19:00:52 +0200

    IUserService IUserRepository

---
+++ IncidentAlert-Management/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using IncidentAlert_Management.Models;
+using System.Linq.Expressions;
+
+namespace IncidentAlert_Management.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetAll();
+        Task<ApplicationUser?> GetByUsername(string username);
+        Task<ApplicationUser?> GetById(string id);
+        Task Add(ApplicationUser user);
+        Task Update(ApplicationUser user);
+        Task DeleteByUsername(string username);
+
+        Task<bool> Exists(Expression<Func<ApplicationUser, bool>> predicate);
+        Task<IEnumerable<ApplicationUser>> FindAll(Expression<Func<ApplicationUser, bool>> predicate);
+        Task<ApplicationUser?> Find(Expression<Func<ApplicationUser, bool>> predicate);
+    }
+}
---
+++ IncidentAlert-Management/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using IncidentAlert_Management.Models.Dto;
+
+namespace IncidentAlert_Management.Services
+{
+    public interface IUserService
+    {
+        Task<ICollection<UserDto>> GetAll();
+        Task Add(UserDto user);
+        Task<string> Login(UserDto loginUser);
+        Task Logout(UserDto logoutUser);
+    }
+}
