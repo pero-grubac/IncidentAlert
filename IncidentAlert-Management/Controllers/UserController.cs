97a83f02ef4680588f1a37fa88f222a6a3e588bd
Pero <perogrubac99@gmail.com>
2024-09-25 19:14:27 +0200

    Authorize on UserController

--- IncidentAlert-Management/Controllers/UserController.cs
+++ IncidentAlert-Management/Controllers/UserController.cs
@@ -1,19 +1,22 @@
 ﻿using IncidentAlert_Management.Models.Dto;
 using IncidentAlert_Management.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IncidentAlert_Management.Controllers
 {
     [Route("users")]
     [ApiController]
+    [Authorize(Roles = "ADMIN")]
+
     public class UserController(IUserService userService) : ControllerBase
     {
         private readonly IUserService _userService = userService;
-        //  TODO login username/password
-        // TODO create username/email/password
+
         [HttpPost("register")]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
+        [AllowAnonymous]
         public async Task<IActionResult> Create([FromBody] CreateUserDto newUser)
         {
             if (newUser == null || !ModelState.IsValid)
@@ -26,6 +29,7 @@ namespace IncidentAlert_Management.Controllers
         [HttpPost("login")]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
+        [AllowAnonymous]
         public async Task<IActionResult> Login([FromBody] LoginDto user)
         {
             if (user == null || !ModelState.IsValid)
