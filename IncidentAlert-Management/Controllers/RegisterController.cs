dc345556ca23101c248948326eee9a25957275ed
Pero <perogrubac99@gmail.com>
2024-09-26 15:53:45 +0200

    RegisterController

---
+++ IncidentAlert-Management/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using IncidentAlert_Management.Models.Dto;
+using IncidentAlert_Management.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IncidentAlert_Management.Controllers
+{
+    [Route("register")]
+    [ApiController]
+    public class RegisterController(IUserService userService) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [AllowAnonymous]
+        public async Task<IActionResult> Create([FromBody] CreateUserDto newUser)
+        {
+            if (newUser == null || !ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _userService.Add(newUser);
+            return Ok("Succesfully created");
+        }
+    }
+}
