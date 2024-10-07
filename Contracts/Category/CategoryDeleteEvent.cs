1d37453892da34a714741d29721ccee419e4bd31
Pero <perogrubac99@gmail.com>
2024-10-08 01:06:44 +0200

    CategoryDeleteEvent CategoryUpdateEvent

---
+++ Contracts/Category/CategoryDeleteEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Contracts.Category
+{
+    public record CategoryDeleteEvent
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Contracts/Category/CategoryUpdateEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Contracts.Category
+{
+    public record CategoryUpdateEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
